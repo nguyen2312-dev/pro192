2a07d5cc7a940009baebb72c4f7542b52b89bd74
nguyen2312-dev <khoinguyenho2006@gmail.com>
2025-06-04 13:17:45 +0700

    Update EmployeeTest.java

--- lab3/3.5/EmployeeTest.java
+++ lab3/3.5/EmployeeTest.java
@@ -16,8 +16,13 @@ public class EmployeeTest {
         System.out.printf("%s %s month salary is: %.1f\n", em2.getfName(), em2.getlName(), em2.getSalary());
         
         //increase salary
-        em1.setSalary(em1.getSalary() * 1.1);
-        em2.setSalary(em2.getSalary() * 1.1);
+        // em1.setSalary(em1.getSalary() * 1.1);
+        // em2.setSalary(em2.getSalary() * 1.1);
+        //raise salary by a method not in main
+
+        em1.raiseSalary();
+        em2.raiseSalary();
+        
         
         System.out.println("\nEmployee salary after raise 10% is \n");
         System.out.printf("%s %s month salary is: %.1f\n", em1.getfName(), em1.getlName(), em1.getSalary());
