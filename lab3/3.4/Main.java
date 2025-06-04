f72f19bd9d124f4846790dca13a74060ae55196b
Khoi Nguyen <khoinguyenho2006@gmail.com>
2025-06-04 13:28:43 +0700

    update lab3

---
+++ lab3/3.4/Main.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author khoin
+ */
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String name = sc.nextLine();
+        
+        System.out.println("Enter quantity: ");
+        int quantity = sc.nextInt();
+        
+        items item = new items(name, quantity);
+        System.out.println("1. Test getName()\n2. Test setQuantity()");
+        System.out.println("Enter TC (1 or 2): ");
+        int valid = sc.nextInt();
+        if(valid == 1){
+            System.out.println("OUTPUT\n"+item.getName().toUpperCase());
+        }else if(valid == 2){
+            System.out.println("Enter new quantity: ");
+            int new_quantity = sc.nextInt();
+            item.setQuantity(new_quantity);
+            System.out.println("OUTPUT\n"+item.getQuantity());
+        }
+    }
+}
